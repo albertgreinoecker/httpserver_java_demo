b193dec598ac2d6659cbdd1d50457aaa78e20e89
Albert Greinoecker <albert.greinoecker@gmail.com>
2021-01-05 21:56:24 +0100

    handler to build a HTML response from file

---
+++ httpserver_demo/src/org/htl/httpserver_demo/handler/HTMLFormHandler.java
@@ -0,0 +1,34 @@
+package org.htl.httpserver_demo.handler;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.htl.httpserver_demo.utils.FileUtils;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+/**
+ * Simple HTML is generated within this handler
+ */
+public class HTMLFormHandler implements HttpHandler {
+
+	@Override
+	public void handle(HttpExchange httpExchange) throws IOException  {
+
+		OutputStream outputStream = httpExchange.getResponseBody();
+		String str = null;
+		try {
+			str = FileUtils.readTextFromClassPath("/org/htl/httpserver_demo/resources/example_form.html");
+			System.out.println(str);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		httpExchange.getResponseHeaders().set("Content-Type", "text/html; charset=UTF-8");
+		httpExchange.sendResponseHeaders(200, str.length()); //200, everything went ok
+		outputStream.write(new String(str.getBytes(), "UTF-8").getBytes());
+		outputStream.flush();
+		outputStream.close();
+	}
+}
